2b862944ac9423cb9fb38c206dd526098dec31a0
aleupons <apahoquei@gmail.com>
2021-07-09 18:35:07 +0200

    Funció per introduir persones vacunades + funcions auxiliars (validar dni i validar data)

---
+++ db/controladors/personas.js
@@ -0,0 +1,53 @@
+const Persona = require("../models/Persona");
+
+const validarDni = (dni) => {
+  const numero = parseInt(dni.slice(0, 7), 10);
+  const letra = dni.slice(-1);
+  const letras = "TRWAGMYFPDXBNJZSQVHLCKET";
+  if (
+    !!numero ||
+    numero > 99999999 ||
+    dni.length !== 9 ||
+    letras.charAt(numero % 23) !== letra.toUpperCase()
+  ) {
+    console.log("DNI incorrecto");
+    return;
+  }
+  return dni;
+};
+
+const validarFecha = (stringFecha) => {
+  const fecha = new Date(stringFecha);
+  if (fecha.toString() === "Invalid Date") {
+    console.log(
+      "Formato de fecha incorrecto. Tiene que tener la forma 'DD/MM/AAAA'"
+    );
+    return;
+  }
+  return fecha;
+};
+
+const introducirPersonaVacunada = async (
+  dni,
+  centroVacunacion,
+  vacuna,
+  fechaPrimeraDosis,
+  fechaSegundaDosis
+) => {
+  try {
+    const nuevaPersona = await Persona.create({
+      dni: validarDni(dni),
+      centroVacunacion,
+      vacuna,
+      fechaPrimeraDosis: validarFecha(fechaPrimeraDosis),
+      ...(vacuna.dosis === 2 && {
+        fechaSegundaDosis: validarFecha(fechaSegundaDosis),
+      }),
+    });
+    return nuevaPersona;
+  } catch (error) {
+    console.log("Error al introducir la persona en la BBDD", error.message);
+  }
+};
+
+module.exports = introducirPersonaVacunada;
